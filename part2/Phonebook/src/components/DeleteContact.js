976228f2a89b52794586100df39b3b4b868b83b1
Raunak <60561127+RnkSngh@users.noreply.github.com>
2023-10-23 19:01:00 -0400

    reorganize project

--- part2/Phonebook/src/components/DeleteContact.js
+++
@@ -1,8 +0,0 @@
-import React, { Component, Fragment, useState } from 'react'
-
-const DeleteContact = ({deleteReq})=>{
-    return (
-        <button onClick={deleteReq}> Delete </button>
-)};
-
-export default DeleteContact
